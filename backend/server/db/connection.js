ec19372d24b554b96925777c7d61dd126feebb0b
jai prashanth <Jaiprashanth003@gmail.com>
2024-05-25 22:37:50 +0530

    first commit

--- backend/server/db/connection.js
+++ backend/server/db/connection.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose');
 mongoose.connect("mongodb+srv://dbreal:dbreal@cluster0.yh3xco3.mongodb.net/signup").then(()=>{
     console.log('connected')
-}).catch(()=>{
+}).catch((e)=>{
     console.log('failed')
+    console.log(e)
 })
-
 
